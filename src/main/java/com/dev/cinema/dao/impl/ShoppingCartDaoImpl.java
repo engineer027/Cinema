94eaf065dbb961a8e51423bd13b82a55626978fa
engineer027 <dp270597dma@gmail.com>
2021-02-03 13:27:21 +0200

    implements ShoppingCartDao

---
+++ src/main/java/com/dev/cinema/dao/impl/ShoppingCartDaoImpl.java
@@ -0,0 +1,50 @@
+package com.dev.cinema.dao.impl;
+
+import com.dev.cinema.dao.ShoppingCartDao;
+import com.dev.cinema.exceptions.DataProcessingException;
+import com.dev.cinema.lib.Dao;
+import com.dev.cinema.model.ShoppingCart;
+import com.dev.cinema.model.User;
+import com.dev.cinema.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+@Dao
+public class ShoppingCartDaoImpl implements ShoppingCartDao {
+    @Override
+    public ShoppingCart add(ShoppingCart shoppingCart) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(shoppingCart);
+            transaction.commit();
+            return shoppingCart;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert shopping cart entity "
+                    + shoppingCart, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public ShoppingCart getByUser(User user) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.get(ShoppingCart.class, user.getId());
+        } catch (Exception e) {
+            throw new RuntimeException("Can't get User with id= " + user.getId(), e);
+        }
+    }
+
+    @Override
+    public void update(ShoppingCart shoppingCart) {
+
+    }
+}
