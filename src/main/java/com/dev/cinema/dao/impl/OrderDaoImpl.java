7fa9744b21b7a1219d5d544870ff197e319095cd
engineer027 <dp270597dma@gmail.com>
2021-02-03 20:18:33 +0200

    create OrderDao and OrderDaoImpl

---
+++ src/main/java/com/dev/cinema/dao/impl/OrderDaoImpl.java
@@ -0,0 +1,51 @@
+package com.dev.cinema.dao.impl;
+
+import com.dev.cinema.dao.OrderDao;
+import com.dev.cinema.exceptions.DataProcessingException;
+import com.dev.cinema.model.Order;
+import com.dev.cinema.model.User;
+import com.dev.cinema.util.HibernateUtil;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+public class OrderDaoImpl implements OrderDao {
+    @Override
+    public Order add(Order order) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(order);
+            transaction.commit();
+            return order;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert order entity "
+                    + order, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Order> getOrdersHistory(User user) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<Order> getMovieSession = session
+                    .createQuery("SELECT o FROM Order o "
+                                    + "left join fetch o.tickets WHERE o.user = :user",
+                            Order.class);
+            getMovieSession.setParameter("user", user);
+            return getMovieSession.getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can`t get orders history where user Id: "
+                    + user.getId(), e);
+        }
+    }
+}
