b351555f5a656ef0b9646238a078b35c20305e6c
engineer027 <dp270597dma@gmail.com>
2021-02-17 15:54:21 +0200

    update Service

--- src/main/java/com/dev/cinema/security/AuthenticationService.java
+++ src/main/java/com/dev/cinema/security/AuthenticationService.java
@@ -1,10 +1,7 @@
 package com.dev.cinema.security;
 
-import com.dev.cinema.exceptions.AuthenticationException;
 import com.dev.cinema.model.User;
 
 public interface AuthenticationService {
-    User login(String login, String password) throws AuthenticationException;
-
     User register(String login, String password);
 }
--- src/main/java/com/dev/cinema/security/AuthenticationServiceImpl.java
+++ src/main/java/com/dev/cinema/security/AuthenticationServiceImpl.java
@@ -1,11 +1,8 @@
 package com.dev.cinema.security;
 
-import com.dev.cinema.exceptions.AuthenticationException;
 import com.dev.cinema.model.User;
 import com.dev.cinema.service.ShoppingCartService;
 import com.dev.cinema.service.UserService;
-import com.dev.cinema.util.HashUtil;
-import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,19 +14,6 @@ public class AuthenticationServiceImpl implements AuthenticationService {
                                      ShoppingCartService shoppingCartService) {
         this.userService = userService;
         this.shoppingCartService = shoppingCartService;
-    }
-
-    @Override
-    public User login(String login, String password) throws AuthenticationException {
-        Optional<User> userFromDB = userService.findByLogin(login);
-        if (userFromDB.isPresent()) {
-            String hashPassword = HashUtil.hashPassword(password, userFromDB.get().getSalt());
-            if (userFromDB.get().getPassword().equals(hashPassword)) {
-                return userFromDB.get();
-            }
-        }
-        throw new AuthenticationException("Incorrect login "
-                + login + "or password" + password);
     }
 
     @Override
